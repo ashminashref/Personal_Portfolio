f7ec70617d03aa2deb8b8a249d894cd0c7c6cc98
ashminashref <ashminashraf07@gmail.com>
2025-10-25 08:19:11 +0530

    new component created

---
+++ src/Components/Home/Tech.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Tech.css'
+import { Container } from 'react-bootstrap'
+function Tech() {
+  return (
+    <div className='mt-5 pt-5'> 
+    <Container className='text-lg-center mt-5 pt-5'>
+        <h1 className='tech-h1'>Passionate about cutting-edge technologies</h1>
+    </Container>
+    </div>
+  )
+}
+
+export default Tech
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Appbar from '../SharedComponents/Appbar'
 import Homehero from '../Components/Home/Homehero'
 import './Home.css'
+import Tech from '../Components/Home/Tech'
 function Home() {
   return (
     <div className='holder'>
          <Appbar/>
          <Homehero/>
+         <Tech/>
     </div>
   )
 }
