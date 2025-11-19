11f1e417680ba0b280282bccf588a2ad32cdad59
ashminashref <ashminashraf07@gmail.com>
2025-11-19 09:26:31 +0530

    temporarily removed skill section for making ui responsiveness better

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Homehero from '../Components/Home/Homehero'
 import './Home.css'
 import Tech from '../Components/Home/Tech'
 import Projects from '../Components/Home/Projects'
-import Skills from '../Components/Home/Skills'
+// import Skills from '../Components/Home/Skills'
 import Footer from '../SharedComponents/Footer'
 import About from '../Components/Home/About'
 function Home() {
@@ -15,7 +15,7 @@ function Home() {
          <Tech/>
          <Projects/>
          <About/>
-         <Skills/>
+         {/* <Skills/> */}
          <Footer/>
     </div>
   )
