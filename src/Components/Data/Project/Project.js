4e63563bb0a5c6dd52b0ffe8235c2345df86783f
ashminashref <ashminashraf07@gmail.com>
2025-11-19 09:21:23 +0530

    added more projects

--- src/Components/Data/Project/Project.js
+++ src/Components/Data/Project/Project.js
@@ -1,7 +1,7 @@
 export const sectionsData = [
   {
     imageUrl: '/Img/project-1.png',
-    title: '-Weaver',
+    title: 'Weaver',
     subtitle: 'A smart web app that converts user prompts into clean CSS code using the Gemini API.',
     description: 'AI-powered web application that transforms natural language into precise, production ready CSS code. Built with React and Vite, it integrates Google Gemini API for intelligent code generation and Bootstrap for a responsive, developer-friendly interface',
     link:"https://github.com/ashminashref/Weaver",
@@ -12,7 +12,7 @@ export const sectionsData = [
   },
   {
     imageUrl: '/Img/project-2.png',
-    title: '-Votte | Digital Voting',
+    title: 'Votte | Digital Voting',
     subtitle: 'A secure online voting platform featuring facial recognition-based voter authentication.',
     description: 'Votte is a secure online voting platform that enables users to vote for their candidates digitally. Its standout feature is facial recognition-based voter authentication, ensuring transparency and preventing fraudulent activities.',
     link:'https://github.com/ashminashref/repovotte',
@@ -29,7 +29,21 @@ export const sectionsData = [
     link:'https://github.com/ashminashref/Forma',
         features:['Redesigned an eCommerce platform focused on showcasing designer chairs, stools, and sofas with a modern, aesthetic interface.',
           'Built using React.js and Bootstrap, ensuring responsive layouts and streamlined product browsing across all devices.'],
-
-
+  },
+   {
+    imageUrl: '/Img/project-4.png',
+    title: 'EchoBoard',
+    subtitle: 'The Unified Digital Platform for Student Concern Submission, Staff Resolution, and Transparent Tracking.',
+    description: 'EchoBoard is a full-stack, responsive web app that simplifies student-staff communication, allowing students to submit concerns and staff to track and resolve them in a centralized dashboard',
+    link:'https://github.com/ashminashref/Echoboard',
+        features:['Simple, guided form for students to file issues across various categories.','Separate, secure login flows for students and staff to ensure data access control and confidentiality','Built with React/Vite and React Bootstrap for a fast, responsive, and professional user experience'],
+  },
+   {
+    imageUrl: '/Img/project-5.png',
+    title: 'Lines of Paar',
+    subtitle: 'A High-Performance, Type-Safe Gallery Showcasing Categorized Drawings.',
+    description: 'Lines of Paar is a modern digital art gallery built with React-Vite and TypeScript, showcasing AI-assisted development and providing a smooth, categorized viewing experience for the artist’s drawings',
+    link:'https://github.com/ashminashref/sublime-art-portfolio',
+        features:['Core structure and components rapidly generated using lovable.dev AI','Drawings are easily filtered and viewed by defined categories','Leverages Vite for optimized, fast asset loading and UX'],
   },
 ];
